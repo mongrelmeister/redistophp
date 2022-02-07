2fcb2a76df66889f33a484ddfddbc302bfac07b3
A. Lopez <mongrelmeister@gmail.com>
2022-02-07 16:46:43 +0100

    Commit para index_redis.php

---
+++ index_redis.php
@@ -0,0 +1,16 @@
+<?php
+  require_once 'hit_counter.php';
+?>
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>your_domain website</title>
+  </head>
+  <body>
+    <h1>Hello World!</h1>
+
+    <p>This is the landing page of <strong>Andrew.NET</strong>.</p>
+  </body>
+</html>
